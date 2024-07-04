7fb0b7ad122c7331fc86654031d4537e7d53836c
satishdonerao <doneraosatish@gmail.com>
2024-07-04 12:34:43 +0530

    first commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2020 - 2024",
+    institution: "DR babasaheb ambedkar technological univercity",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
     duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    institution: "Netaji subhashchandrabose jr.college nanded",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    institution: "MJP kinwat,nanded",
   }
 ]
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,15 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: ' trainee Software Engineer ',
+    company: "Sumago Infotech Private Ltd.",
+    duration: "(Feb 2024 - Present)"
   },
   {
     id: 2,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    duration: "(Jun 2023 - Jan 2024)"
   },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
+ 
 ]
